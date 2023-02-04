fe89a4284aa02a05505c0785cb3859b0783e9701
kayden naganna <123kayden@gmail.com>
2023-02-04 13:18:39 +0200

    sorted map layout

--- ForSmart2/wwwroot/js/site.js
+++ ForSmart2/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    //$(".map").hide();
+
+    $(".map").off().on("click", function (e) {
+        var posX = $(this).offset().left, posY = $(this).offset().top;
+        alert((e.pageX - posX) + ' , ' + (e.pageY - posY));
+    });
+});
+
+
+$(document).ready(function () { });
