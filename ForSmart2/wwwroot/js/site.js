fdb14cf88d7704f4c25cb7b7467e9539d4b4cee8
kayden naganna <123kayden@gmail.com>
2023-02-07 06:22:10 +0200

    start creating map nodes

--- ForSmart2/wwwroot/js/site.js
+++ ForSmart2/wwwroot/js/site.js
@@ -2,12 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var isRouteCreate = false;
+
 $(function () {
     //$(".map").hide();
 
     $(".map").off().on("click", function (e) {
         var posX = $(this).offset().left, posY = $(this).offset().top;
-        alert((e.pageX - posX) + ' , ' + (e.pageY - posY));
+        //alert((e.pageX - posX) + ' , ' + (e.pageY - posY));
+
+        var relx = e.pageX - posX;
+        var rely = e.pageY - posY;
+
+        $(".map-container").append("<div class='station'></div>");
+
+
     });
 });
 
